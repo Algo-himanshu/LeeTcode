1c8f0376f501f85c00797ff900cba937eff19afd
Himanshu tiwari <44333537+Algo-himanshu@users.noreply.github.com>
2022-09-15 20:00:11 +0530

    Time: 1 ms (66.44%), Space: 42.3 MB (42.06%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -22,14 +22,11 @@ class Solution {
                     }
                 
                 }
+                else
+                    {if(obstacleGrid[i][j]==1) dp[i][j]=0;
+            else dp[i][j]=dp[i-1][j]+dp[i][j-1];}
             }
         }
-        for(int i = 1;i<m;i++){
-            for(int j = 1;j<n;j++){
-            if(obstacleGrid[i][j]==1) dp[i][j]=0;
-            else dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-return dp[m-1][n-1];
+    return dp[m-1][n-1];
     }
 }
